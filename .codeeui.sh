cc176307339e89b940ec5d0479bb5febedd2eb1e
李光春 <gc@dtapp.net>
2021-06-25 15:34:37 +0800

    tra

---
+++ .codeeui.sh
@@ -0,0 +1,13 @@
+# 使用vendor文件夹构建
+export GO15VENDOREXPERIMENT=1
+export GOPROXY=https://goproxy.cn,direct
+# 在工作目录创建源文件夹
+mkdir -p $GOPATH/src/dtapps/golang/
+# 拷贝代码到创建好的目录
+cp -rf . $GOPATH/src/dtapps/golang/
+# 进入项目
+cd $GOPATH/src/dtapps/golang/
+# 构建，在GOPATH下生成构建包
+go mod tidy
+# 列出文件
+ls -lh
