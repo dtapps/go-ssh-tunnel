d29a6013030e822f893e12fe730d75b12baa322a
李光春 <gc@dtapp.net>
2021-06-26 11:15:30 +0800

    update

--- .codeeui.sh
+++ .codeeui.sh
@@ -2,11 +2,11 @@
 export GO15VENDOREXPERIMENT=1
 export GOPROXY=https://goproxy.cn,direct
 # 在工作目录创建源文件夹
-mkdir -p $GOPATH/src/dtapps/golang/
+mkdir -p $GOPATH/src/dtapps/
 # 拷贝代码到创建好的目录
-cp -rf . $GOPATH/src/dtapps/golang/
+cp -rf . $GOPATH/src/dtapps/
 # 进入项目
-cd $GOPATH/src/dtapps/golang/
+cd $GOPATH/src/dtapps/
 # 构建，在GOPATH下生成构建包
 go mod tidy
 # 列出文件
